80f30055db3442ed33759a77365de2798f3518a6
user <user@DESKTOP-B366R1N>
2026-02-13 14:01:02 +0100

    Actualizasao

--- PROYECTOREPOSITORIO1/src/paqueterepositorio1/claserepositorio1.java
+++ PROYECTOREPOSITORIO1/src/paqueterepositorio1/claserepositorio1.java
@@ -5,7 +5,7 @@ public class claserepositorio1 {
 	public static void main(String[] args) {
 		int ili = 3;
 		
-		System.out.println("Pringao");
+		System.out.println("Pringao pringaisimo");
 		System.out.println(ili);
 		
 	}
